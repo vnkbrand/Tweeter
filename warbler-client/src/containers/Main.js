1f919da994b7b9b1bd58557e59c9c7e93d9aabea
Elie Schoppik <eschoppik@gmail.com>
2018-03-17 13:52:21 -0700

    right before messagelist

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -3,41 +3,64 @@ import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
 import AuthForm from "../components/AuthForm";
+import { authUser } from "../store/actions/auth";
+import { removeError } from "../store/actions/errors";
 
-const Main = () => (
-  <div className="container">
-    <Switch>
-      <Route exact path="/" render={props => <Homepage {...props} />} />
-      <Route
-        exact
-        path="/signin"
-        render={props => {
-          return (
-            <AuthForm buttonText="Log in" heading="Welcome Back." {...props} />
-          );
-        }}
-      />
-      <Route
-        exact
-        path="/signup"
-        render={props => {
-          return (
-            <AuthForm
-              buttonText="Sign me up!"
-              heading="Join Warbler today."
-              {...props}
-            />
-          );
-        }}
-      />
-    </Switch>
-  </div>
-);
+const Main = props => {
+  const { authUser, errors, removeError, currentUser } = props;
+  return (
+    <div className="container">
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props => <Homepage currentUser={currentUser} {...props} />}
+        />
+        <Route
+          exact
+          path="/signin"
+          render={props => {
+            return (
+              <AuthForm
+                removeError={removeError}
+                errors={errors}
+                onAuth={authUser}
+                buttonText="Log in"
+                heading="Welcome Back."
+                {...props}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={props => {
+            return (
+              <AuthForm
+                removeError={removeError}
+                errors={errors}
+                onAuth={authUser}
+                signUp
+                buttonText="Sign me up!"
+                heading="Join Warbler today."
+                {...props}
+              />
+            );
+          }}
+        />
+      </Switch>
+    </div>
+  );
+};
 
 function mapStateToProps(state) {
   return {
-    currentUser: state.currentUser
+    currentUser: state.currentUser,
+    errors: state.errors
   };
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(
+  connect(mapStateToProps, { authUser, removeError })(Main)
+);
